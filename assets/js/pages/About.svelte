bb4c81985e0ca3545cdf25c49cbbaec7cc64b453
Damian972 <25445518+Damian972@users.noreply.github.com>
2020-10-27 17:10:20 -0400

    feat: about page

---
+++ assets/js/pages/About.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Layout from '../@shared/Layout.svelte';
+
+	export let title = 'About';
+</script>
+
+<Layout {title}>
+	<h1>About page</h1>
+</Layout>
---
+++ src/Controller/AboutController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Rompetomp\InertiaBundle\Service\InertiaInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/about")
+ */
+class AboutController extends AbstractController
+{
+    /**
+     * @Route("", name="about")
+     */
+    public function index(InertiaInterface $inertia): Response
+    {
+        return $inertia->render('About');
+    }
+}
